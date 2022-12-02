b77248cac2c53d34451d00605bc626c093f42a93
IsraCrocus <ipuyol@uoc.edu>
2022-12-02 20:07:39 +0100

    varios cambios

--- src/tkim/dao/IPedidoDAO.java
+++ src/tkim/dao/IPedidoDAO.java
@@ -1,5 +1,20 @@
 package tkim.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import tkim.modelo.Articulo;
+import tkim.modelo.Cliente;
+import tkim.modelo.Pedido;
+
 public interface IPedidoDAO {
+	
+public Boolean existePedido(Integer codigo);
+	
+	public String addPedido(int numeroPedido, int unidadesPedido, LocalDateTime fechaHoraPedido, Cliente cliente,
+			Articulo articulo);
+	public String eliminarPedido(int numPedido);
+	public List<Pedido> pedidosEnviados(String nif);
+	public List<Pedido> pedidosPendientes(String nif);
 
 }
--- src/tkim/dao/PedidoDAO.java
+++ src/tkim/dao/PedidoDAO.java
@@ -1,5 +1,43 @@
 package tkim.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import tkim.modelo.Articulo;
+import tkim.modelo.Cliente;
+import tkim.modelo.Pedido;
+
 public class PedidoDAO implements IPedidoDAO{
+
+	@Override
+	public Boolean existePedido(Integer codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addPedido(int numeroPedido, int unidadesPedido, LocalDateTime fechaHoraPedido, Cliente cliente,
+			Articulo articulo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarPedido(int numPedido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pedido> pedidosEnviados(String nif) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pedido> pedidosPendientes(String nif) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
